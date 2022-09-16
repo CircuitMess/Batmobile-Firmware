e4a97ce09d738783015c0a2290a3c3ad6bf4faa9
Emil Gajšak <emil.gajsak@gmail.com>
2022-09-16 13:08:22 +0200

    Added network config, data model structs

---
+++ src/NetworkConfig.cpp
@@ -0,0 +1,6 @@
+#include "NetworkConfig.h"
+
+IPAddress batmobileIP(0, 0, 0, 5);
+IPAddress controllerIP(0, 0, 0, 4);
+IPAddress gateway(0, 0, 0, 1);
+IPAddress subnet(255, 255, 255, 0);
---
+++ src/NetworkConfig.h
@@ -0,0 +1,12 @@
+#ifndef BATMOBILE_FIRMWARE_NETWORKCONFIG_H
+#define BATMOBILE_FIRMWARE_NETWORKCONFIG_H
+
+#include <WiFi.h>
+
+extern IPAddress batmobileIP;
+extern IPAddress controllerIP;
+extern IPAddress gateway;
+extern IPAddress subnet;
+
+
+#endif //BATMOBILE_FIRMWARE_NETWORKCONFIG_H
