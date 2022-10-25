99708fbe37d8d3a7780723ea13eba082877127ab
subo <gamedevsubo@gmail.com>
2022-10-25 12:36:42 +0200

    Add empty states classes

---
+++ src/States/IdleState.cpp
@@ -0,0 +1,17 @@
+#include "IdleState.h"
+
+IdleState::IdleState(){
+
+}
+
+IdleState::~IdleState(){
+
+}
+
+void IdleState::onStart(){
+	State::onStart();
+}
+
+void IdleState::onStop(){
+	State::onStop();
+}
