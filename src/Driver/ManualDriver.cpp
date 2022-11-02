a4c3550c0a9bd018ca578cf60c8a501ad47dcc91
Emil Gajšak <emil.gajsak@gmail.com>
2022-11-02 15:52:40 +0100

    Added ManualDriver as ComListener

--- src/Driver/ManualDriver.cpp
+++ src/Driver/ManualDriver.cpp
@@ -1,5 +1,52 @@
 #include "ManualDriver.h"
+#include <Communication/Communication.h>
+#include <Batmobile.h>
+
+ManualDriver::ManualDriver(){
+	Com.addListener({ComType::Boost, ComType::DriveDir}, this);
+}
 
 void ManualDriver::onFrame(DriveInfo& driveInfo){
 
+}
+
+void ManualDriver::onBoost(bool boost){
+	boosting = boost;
+	setMotors();
+}
+
+void ManualDriver::onDriveDir(DriveDirection dir){
+	direction = dir;
+	setMotors();
+}
+
+void ManualDriver::setMotors(){
+	int8_t leftSpeed, rightSpeed;
+
+	switch(direction){
+		case DriveDirection::Forward:
+			leftSpeed = rightSpeed = speedStraight;
+			break;
+		case DriveDirection::Backward:
+			leftSpeed = rightSpeed = -speedStraight;
+			break;
+		case DriveDirection::Left:
+			leftSpeed = speedTurnInner;
+			rightSpeed = speedTurnOuter;
+			break;
+		case DriveDirection::Right:
+			leftSpeed = speedTurnOuter;
+			rightSpeed = speedTurnInner;
+			break;
+		default:
+			leftSpeed = rightSpeed = 0;
+			break;
+	}
+
+	if(!boosting){
+		leftSpeed *= noBoostMultiplier;
+		rightSpeed *= noBoostMultiplier;
+	}
+
+	Motors.setAll({rightSpeed, leftSpeed, rightSpeed, leftSpeed});
 }
--- src/Driver/ManualDriver.h
+++ src/Driver/ManualDriver.h
@@ -2,12 +2,27 @@
 #define BATMOBILE_FIRMWARE_MANUALDRIVER_H
 
 #include "Driver.h"
+#include <Communication/ComListener.h>
 
 //TODO - register to Comms and forward controls to motors, taking boost into account
 
-class ManualDriver : public Driver {
+class ManualDriver : public Driver , private ComListener{
 public:
+	ManualDriver();
 	void onFrame(DriveInfo& driveInfo) override;
+
+private:
+	void onBoost(bool boost) override;
+	void onDriveDir(DriveDirection dir) override;
+	void setMotors();
+
+	bool boosting = false;
+	DriveDirection direction{};
+
+	static constexpr uint8_t speedStraight = 100;
+	static constexpr uint8_t speedTurnOuter = 100;
+	static constexpr uint8_t speedTurnInner = 20;
+	static constexpr float noBoostMultiplier = 0.7;
 };
 
 
