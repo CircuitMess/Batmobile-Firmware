b2c3c7eccb46f9f11ed61f7b5cc990b67bef0d6d
Filip Budiša <filip@circuitmess.com>
2022-11-03 14:16:54 +0100

    Start PairState in setup

--- Batmobile-Firmware.ino
+++ Batmobile-Firmware.ino
@@ -6,23 +6,29 @@
 #include <Wheelson.h>
 #include <Camera.h>
 #include <Loop/LoopManager.h>
-#include "src/States/PairService.h"
+#include "src/States/State.h"
+#include "src/States/PairState.h"
 
 Sprite *baseSprite;
 Display *display;
-Pair::PairService* pairService;
+
+State* state = nullptr;
+
 void setup() {
     Serial.begin(115200);
-    Nuvo.begin();
 
     Batmobile.begin();
     Wheelson.begin();
 
     display = &Wheelson.getDisplay();
     baseSprite = display->getBaseSprite();
+
     baseSprite->clear(TFT_GREEN);
+	display->commit();
     LED.setBacklight(true);
-    pairService = new Pair::PairService();
+
+    state = new PairState();
+	state->start();
 }
 
 void loop() {
