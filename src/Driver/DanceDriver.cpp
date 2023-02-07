1ce937022ad3e67991c84e82ac85a567d481e501
subo <gamedevsubo@gmail.com>
2023-02-07 16:19:33 +0100

    Add blinking colors to dance

--- src/Driver/DanceDriver.cpp
+++ src/Driver/DanceDriver.cpp
@@ -8,6 +8,11 @@ static const MotorInfo danceInfo[5][2] = {{{ 0,    0,   0,    0 },    { 0,   0, 
 								   {{ 60,   60,  60,   60 },   { -60, -60,  -60, -60 }},
 								   {{ -100, 100, -100, 100 },  { 100, -100, 100, -100 }},
 								   {{ -65,  -45, -65,  -45 }, { -45, -65,  -45, -65 }}};
+static const glm::vec3 colors[5][2] = {{{ 0,   0,   0 },   { 0,   0,   0 }},
+								{{ 65,  105, 225 }, { 255, 229, 180 }}, //royal blue & peach
+								{{ 54,  69,  79 },  { 255, 255, 0 }},  //Charcoal & yellow
+								{{ 120, 190, 33 },  { 89,  203, 232 }}, //Lime green & electric blue
+								{{ 255, 240, 245 }, { 0,   128, 128 }}, }; //Lavender & teal
 
 DanceDriver::DanceDriver() : Driver(DriveMode::Dance){ }
 
@@ -36,6 +41,7 @@ void DanceDriver::onDance(DanceType dance) {
 	count = 0;
 	danceFlag = false;
 	Motors.setAll(danceInfo[(uint8_t)currentDance][0]);
+	Underlights.blinkContinuous(colors[(uint8_t)currentDance][0]);
 }
 
 void DanceDriver::loop(uint micros) {
@@ -45,9 +51,11 @@ void DanceDriver::loop(uint micros) {
         if(danceFlag){
             danceFlag = false;
             Motors.setAll(danceInfo[(uint8_t)currentDance][0]);
-        }else{
+			Underlights.blinkContinuous(colors[(uint8_t)currentDance][0]);
+		}else{
             danceFlag = true;
             Motors.setAll(danceInfo[(uint8_t)currentDance][1]);
+			Underlights.blinkContinuous(colors[(uint8_t)currentDance][1]);
         }
     }
 }
