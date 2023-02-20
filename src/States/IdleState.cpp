755772b6b606434505404e0724e19d2c230ebb35
Emil Gajšak <emil.gajsak@gmail.com>
2023-02-20 14:52:56 +0100

    SFX preview in IdleState (SimpleProg)

--- src/States/IdleState.cpp
+++ src/States/IdleState.cpp
@@ -12,7 +12,7 @@ IdleState::~IdleState(){
 void IdleState::onStart(){
 	Underlights.breathe({0, 255, 50}, {120, 0, 255}, 6000);
 	LoopManager::addListener(this);
-	Com.addListener(ComType::SettingsSound, this);
+	Com.addListener({ComType::SettingsSound, ComType::SoundEffect}, this);
 	srand(time(NULL));
 	threshold = (rand() % 11 + 10) * 1000000; //10-20 sec
 }
@@ -29,10 +29,20 @@ void IdleState::onSettingsSound(){
 
 void IdleState::loop(uint micros){
 	counter += micros;
-	if(counter >= threshold){
+	if(counter >= threshold && !Audio.isPlaying()){
 		counter = 0;
 		uint8_t honkRand = rand() % 5;
 		Audio.play(SPIFFS.open(String("/SFX/idle") + honkRand + ".aac"));
 		threshold = (rand() % 11 + 10) * 1000000; //10-20 sec
 	}
+}
+
+void IdleState::onSoundEffect(uint8_t sample){
+	File root = SPIFFS.open("/SFX");
+	File f = root.openNextFile();
+	for(int j = 0; j < sample + 1; ++j){
+		f = root.openNextFile();
+		if(!f) return;
+	}
+	Audio.play(f);
 }
