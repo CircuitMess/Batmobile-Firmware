2ee85f35708b88d8eff333e7c9b6b6fa630bab92
subo <gamedevsubo@gmail.com>
2023-02-23 16:24:17 +0100

    Use Lightshow instead of blinking lights in DanceDriver

--- src/Driver/DanceDriver.cpp
+++ src/Driver/DanceDriver.cpp
@@ -14,15 +14,7 @@ static constexpr MotorInfo danceInfo[9][2] = {{{ 0,    0,   0,    0 },   { 0,   
 											  {{ 40,   90,  40,   90 },  { 40,   90,   40,   90 }}, //WaxOff
 											  {{ 35,   35,  35,   35 },  { 0,    0,    0,    0 }}};     //Sneak
 
-static constexpr glm::vec3 colors[9][2] = {{{ 0,   0,   0 },   { 0,   0,   0 }},
-										   {{ 65,  105, 225 }, { 255, 229, 180 }}, //royal blue & peach
-										   {{ 54,  69,  79 },  { 255, 255, 0 }},  //Charcoal & yellow
-										   {{ 120, 190, 33 },  { 89,  203, 232 }}, //Lime green & electric blue
-										   {{ 255, 240, 245 }, { 0,   128, 128 }}, //Lavender & teal
-										   {{ 137, 207, 240 }, { 255, 255, 255 }}, //Baby blue & white
-										   {{ 218, 112, 214 }, { 222, 192, 95 }}, //Orchid blue & Cream Gold
-										   {{ 120, 81,  169 }, { 199, 211, 212 }}, //Royal Purple & Ice Flow
-										   {{ 214, 157, 48 },  { 97,  98,  71 }}}; //Mango Mojito & Terrarium Moss
+
 
 DanceDriver::DanceDriver() : Driver(DriveMode::Dance){}
 
@@ -52,8 +44,9 @@ void DanceDriver::onDance(DanceType dance){
 	count = 0;
 	danceFlag = false;
 	Motors.setAll(danceInfo[(uint8_t) currentDance][0]);
-	Underlights.blinkContinuous(colors[(uint8_t) currentDance][0]);
-	Audio.play(SPIFFS.open("/SFX/beep.aac"));
+	if(lightshow) lightshow->stop();
+	lightshow = Lightshow::createLightshow((LightshowType) (uint8_t) currentDance);
+	if(lightshow) lightshow->start();
 }
 
 void DanceDriver::loop(uint micros){
@@ -63,13 +56,9 @@ void DanceDriver::loop(uint micros){
 		if(danceFlag){
 			danceFlag = false;
 			Motors.setAll(danceInfo[(uint8_t) currentDance][0]);
-			Audio.play(SPIFFS.open("/SFX/beep.aac"));
-			Underlights.blinkContinuous(colors[(uint8_t) currentDance][0]);
 		}else{
 			danceFlag = true;
 			Motors.setAll(danceInfo[(uint8_t) currentDance][1]);
-			Audio.play(SPIFFS.open("/SFX/beep.aac"));
-			Underlights.blinkContinuous(colors[(uint8_t) currentDance][1]);
 		}
 	}
 }
--- src/Driver/DanceDriver.h
+++ src/Driver/DanceDriver.h
@@ -19,6 +19,8 @@ private:
 	void onDance(DanceType dance) override;
 	void loop(uint micros) override;
 
+	std::unique_ptr<Lightshow> lightshow;
+
 	static constexpr uint32_t switchDelay = 1500000;
 	uint32_t count = 0;
 	bool danceFlag = true;
