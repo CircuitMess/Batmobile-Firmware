78f42b9d9eadea9e1936d0d230624db10e9a39e9
Ernest Subotić <93708543+Subo00@users.noreply.github.com>
2023-03-08 16:55:13 +0100

    More dance modes bat 134 (#25)
    * Move danceInfo from .h to .cpp in DanceDriver
    
    * Add blinking colors to dance
    
    * Add beep sound to DanceDriver
    
    * Added constexpr to dance constants
    
    * Make switchDelay into a static constexpr in DanceDriver
    
    * Add dance information for Waltz and change previous dances a bit
    
    * Add dance information for Wax on, Wax off and sneak
    
    ---------
    
    Co-authored-by: Emil Gajšak <emil.gajsak@gmail.com>
    Co-authored-by: Filip Budiša <filip@circuitmess.com>

--- src/Driver/DanceDriver.cpp
+++ src/Driver/DanceDriver.cpp
@@ -4,10 +4,30 @@
 #include <Batmobile.h>
 
 
-DanceDriver::DanceDriver() : Driver(DriveMode::Dance){ }
+static constexpr MotorInfo danceInfo[9][2] = {{{ 0,    0,   0,    0 },   { 0,    0,    0,    0 }}, //Idle
+											  {{ 60,   -60, 80,   -80 }, { -60,  60,   -80,  80 }}, //Wiggle
+											  {{ 60,   60,  60,   60 },  { -60,  -60,  -60,  -60 }}, //BackNFront
+											  {{ -100, 100, -100, 100 }, { 100,  -100, 100,  -100 }}, //Spin
+											  {{ -50,  -50, -30,  -20 }, { -50,  -50,  -20,  -30 }}, //BackItUp
+											  {{ 25,   100, 35,   100 }, { -100, -25,  -100, -35 }}, //Waltz
+											  {{ 90,   40,  90,   40 },  { 90,   40,  90,   40 }}, //WaxOn
+											  {{ 40,   90,  40,   90 },  { 40,   90,   40,   90 }}, //WaxOff
+											  {{ 35,   35,  35,   35 },  { 0,    0,    0,    0 }}};     //Sneak
 
-DanceDriver::~DanceDriver() {
-    stop();
+static constexpr glm::vec3 colors[9][2] = {{{ 0,   0,   0 },   { 0,   0,   0 }},
+										   {{ 65,  105, 225 }, { 255, 229, 180 }}, //royal blue & peach
+										   {{ 54,  69,  79 },  { 255, 255, 0 }},  //Charcoal & yellow
+										   {{ 120, 190, 33 },  { 89,  203, 232 }}, //Lime green & electric blue
+										   {{ 255, 240, 245 }, { 0,   128, 128 }}, //Lavender & teal
+										   {{ 137, 207, 240 }, { 255, 255, 255 }}, //Baby blue & white
+										   {{ 218, 112, 214 }, { 222, 192, 95 }}, //Orchid blue & Cream Gold
+										   {{ 120, 81,  169 }, { 199, 211, 212 }}, //Royal Purple & Ice Flow
+										   {{ 214, 157, 48 },  { 97,  98,  71 }}}; //Mango Mojito & Terrarium Moss
+
+DanceDriver::DanceDriver() : Driver(DriveMode::Dance){}
+
+DanceDriver::~DanceDriver(){
+	stop();
 }
 
 void DanceDriver::onStart(){
@@ -22,28 +42,34 @@ void DanceDriver::onStop(){
 	LoopManager::removeListener(this);
 }
 
-void DanceDriver::onFrame(DriveInfo &driveInfo) {
+void DanceDriver::onFrame(DriveInfo& driveInfo){
 
 }
 
-void DanceDriver::onDance(DanceType dance) {
-    currentDance = dance;
+void DanceDriver::onDance(DanceType dance){
+	currentDance = dance;
 
 	count = 0;
 	danceFlag = false;
-	Motors.setAll(danceInfo[(uint8_t)currentDance][0]);
+	Motors.setAll(danceInfo[(uint8_t) currentDance][0]);
+	Underlights.blinkContinuous(colors[(uint8_t) currentDance][0]);
+	Audio.play(SPIFFS.open("/SFX/beep.aac"));
 }
 
-void DanceDriver::loop(uint micros) {
-    count += micros;
-    if(count >= switchDelay){
-        count = 0;
-        if(danceFlag){
-            danceFlag = false;
-            Motors.setAll(danceInfo[(uint8_t)currentDance][0]);
-        }else{
-            danceFlag = true;
-            Motors.setAll(danceInfo[(uint8_t)currentDance][1]);
-        }
-    }
+void DanceDriver::loop(uint micros){
+	count += micros;
+	if(count >= switchDelay){
+		count = 0;
+		if(danceFlag){
+			danceFlag = false;
+			Motors.setAll(danceInfo[(uint8_t) currentDance][0]);
+			Audio.play(SPIFFS.open("/SFX/beep.aac"));
+			Underlights.blinkContinuous(colors[(uint8_t) currentDance][0]);
+		}else{
+			danceFlag = true;
+			Motors.setAll(danceInfo[(uint8_t) currentDance][1]);
+			Audio.play(SPIFFS.open("/SFX/beep.aac"));
+			Underlights.blinkContinuous(colors[(uint8_t) currentDance][1]);
+		}
+	}
 }
--- src/Driver/DanceDriver.h
+++ src/Driver/DanceDriver.h
@@ -7,26 +7,22 @@
 
 class DanceDriver : public Driver, private ComListener, private LoopListener {
 public:
-    DanceDriver();
-    ~DanceDriver() override;
-    void onFrame(DriveInfo &driveInfo) override;
+	DanceDriver();
+	~DanceDriver() override;
+	void onFrame(DriveInfo& driveInfo) override;
 
-private:
-    void onDance(DanceType dance) override;
-    void loop(uint micros) override;
 protected:
 	void onStart() override;
 	void onStop() override;
-private:
 
-	MotorInfo danceInfo[4][2] = {{{0,   0,   0,   0},   {0,   0,   0,   0}},
-                                 {{40,  -40, 40,  -40}, {-40, 40,  -40, 40}},
-                                 {{60,  60,  60,  60},  {-60, -60, -60, -60}},
-                                 {{-100, 100,  -100, 100},  {100,  -100, 100,  -100}}};
-    uint32_t switchDelay = 1500000;
-    uint32_t count = 0;
-    bool danceFlag = true;
-    DanceType currentDance = DanceType::Idle;
+private:
+	void onDance(DanceType dance) override;
+	void loop(uint micros) override;
+
+	static constexpr uint32_t switchDelay = 1500000;
+	uint32_t count = 0;
+	bool danceFlag = true;
+	DanceType currentDance = DanceType::Idle;
 };
 
 
